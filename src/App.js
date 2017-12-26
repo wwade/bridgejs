dd351a680fdb181f574b485dd61eb0b26b824c6a
Wade Carpenter <wade@arista.com>
2017-12-26 12:34:07 -0800

    wip

--- src/App.js
+++ src/App.js
@@ -7,6 +7,55 @@ import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import SessionListContainer from "./comp/SessionList";
+import request from "superagent";
+import {List, ListItem} from 'material-ui/List';
+
+class SessionBoards extends Component {
+   render() {
+      if (!this.props.boards) {
+         return "";
+      } else {
+         return (
+            <List>
+            {this.props.boards.map( (b, index ) => {
+               return (
+                  <ListItem key={index} value={index} primaryText={b.Team} />
+               );
+             })}
+            </List>
+         );
+      }
+   }
+}
+
+class SessionBoardsContainer extends Component {
+   state = {
+      boards: null,
+      sessionId: null
+   }
+
+   loadBoards(sessionId) {
+      this.setState({sessionId: sessionId});
+      request.get("http://192.168.0.16/boards?sessionId=" + sessionId).end((err, res) => {
+         this.setState({boards: res.body.BoardSets});
+      });
+   }
+
+   componentDidMount() {
+      this.loadBoards(this.props.session.Id);
+   }
+
+   componentWillReceiveProps(nextProps) {
+      let sessionId = nextProps.session.Id;
+      if (sessionId !== this.state.sessionId) {
+         this.loadBoards(nextProps.session.Id);
+      }
+   }
+
+   render() {
+      return <SessionBoards boards={this.state.boards} />
+   }
+}
 
 class SessionCard extends Component {
    render() {
@@ -15,6 +64,7 @@ class SessionCard extends Component {
          <Card>
             <CardTitle title={this.props.session.Name} />
             <CardText>
+               <SessionBoardsContainer session={this.props.session} />
                {this.props.session.dateString()}
             </CardText>
          </Card>
@@ -43,7 +93,11 @@ class AppMain extends Component {
             <AppBar
                onLeft={this.onLeft}
                title="Bridge Scores" />
-            <Drawer open={this.state.open}>
+            <Drawer
+               docked={false}
+               open={this.state.open}
+               onRequestChange={(open) => this.setState({open})}
+            >
                <SessionListContainer onSelection={this.onSelection}/>
             </Drawer>
             <SessionCard session={this.state.session} />
