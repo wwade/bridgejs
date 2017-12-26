804eb94ebaf3ca387a43b02b907294e355d68a11
Wade Carpenter <wade.carpenter+github@gmail.com>
2017-12-25 23:07:19 -0800

    wip

---
+++ src/model/Data.js
@@ -0,0 +1,19 @@
+import request from "superagent";
+
+export class SessionObject {
+   constructor( jSess ) {
+      this.Id = jSess.Id;
+      this.Timestamp = jSess.Timestamp;
+      this.Name = jSess.Name;
+      this.date = new Date( this.Timestamp );
+   }
+
+   compare(other) {
+      return other.Timestamp - this.Timestamp;
+   }
+
+   dateString() {
+      let date = new Date( this.Timestamp );
+      return date.toUTCString()
+   }
+}
