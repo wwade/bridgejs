1bfe9ff6a65ed4173f27dccdfa653068b97e8287
Wade Carpenter <wade@arista.com>
2017-12-26 16:45:09 -0800

    wip lint

--- src/ui/AppBar.js
+++ src/ui/AppBar.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import AppBar from 'material-ui/AppBar';
+import React from "react";
+import AppBar from "material-ui/AppBar";
+import PropTypes from "prop-types";
 
 const Main = (props) => (
    <AppBar
     onLeftIconButtonClick={props.onLeft}
     title={props.title} />
 );
+
+Main.propTypes = {
+   onLeft: PropTypes.func.isRequired,
+   title: PropTypes.string.isRequired
+};
 
 export default Main;
