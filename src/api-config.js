f533e239ac5b9f009e8960480d55650ffe49ed9a
Wade Carpenter <wwade@users.noreply.github.com>
2020-12-15 23:07:05 -0800

    Delete all the files

--- src/api-config.js
+++
@@ -1,9 +0,0 @@
-let backendHost;
-
-if (process.env.NODE_ENV === "production") {
-   backendHost = "http://bridge.trevr.xyz/";
-} else {
-   backendHost = "http://localhost/";
-}
-
-export const API_ROOT = `${backendHost}`;
